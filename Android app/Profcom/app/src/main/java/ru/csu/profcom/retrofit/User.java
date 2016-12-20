101b3b3dc0464eaf46eb0b7faadfb4da3f01e12b
shad <shadow.mr96@gmail.com>
2016-12-20 12:29:09 +0500

    added User interface and User object for retrofit+GsonConverter

---
+++ Android app/Profcom/app/src/main/java/ru/csu/profcom/retrofit/User.java
@@ -0,0 +1,141 @@
+package ru.csu.profcom.retrofit;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("login")
+    @Expose
+    private String login;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("firstName")
+    @Expose
+    private String firstName;
+    @SerializedName("lastName")
+    @Expose
+    private String lastName;
+    @SerializedName("surName")
+    @Expose
+    private String surName;
+    @SerializedName("group")
+    @Expose
+    private String group;
+    @SerializedName("budget")
+    @Expose
+    private Boolean budget;
+    @SerializedName("fulltime")
+    @Expose
+    private Boolean fulltime;
+    @SerializedName("tabNum")
+    @Expose
+    private String tabNum;
+    @SerializedName("avatar")
+    @Expose
+    private Object avatar;
+    @SerializedName("feePay")
+    @Expose
+    private Boolean feePay;
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Boolean getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Boolean budget) {
+        this.budget = budget;
+    }
+
+    public Boolean getFulltime() {
+        return fulltime;
+    }
+
+    public void setFulltime(Boolean fulltime) {
+        this.fulltime = fulltime;
+    }
+
+    public String getTabNum() {
+        return tabNum;
+    }
+
+    public void setTabNum(String tabNum) {
+        this.tabNum = tabNum;
+    }
+
+    public Object getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Object avatar) {
+        this.avatar = avatar;
+    }
+
+    public Boolean getFeePay() {
+        return feePay;
+    }
+
+    public void setFeePay(Boolean feePay) {
+        this.feePay = feePay;
+    }
+
+}
---
+++ Android app/Profcom/app/src/main/java/ru/csu/profcom/retrofit/UserAPI.java
@@ -0,0 +1,33 @@
+package ru.csu.profcom.retrofit;
+
+import android.media.Image;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface UserAPI {
+    @GET("/User/autorizate/{login}/{password}")
+    Call<Long> authorize(@Path("login") String login, @Path("password") String password);
+
+    @POST("/User")
+    Call<User> registerUser(
+            @Header("Content-Type") String contentType,
+            @Body User user
+            /*@Field("login") String login,
+            @Field("password") String password,
+            @Field("firstName") String firstName,
+            @Field("lastName") String lastName,
+            @Field("surName") String surName,
+            @Field("budget") Boolean budget,
+            @Field("fulltime") Boolean fulltime,
+            @Field("feePay") Boolean feePay*/
+            //@Field("group") String group
+    );
+}
