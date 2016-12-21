514b40b1b896915089efdf5805b930b10cac80c6
shad <shadow.mr96@gmail.com>
2016-12-21 23:50:21 +0500

    added field UserInfoStorage to class and FragmentCategories

--- Android app/Profcom/app/src/main/java/ru/csu/profcom/MainActivity.java
+++ Android app/Profcom/app/src/main/java/ru/csu/profcom/MainActivity.java
@@ -19,16 +19,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, FragmentQuestions.OnFragmentInteractionListener, FragmentPersonalArea.OnFragmentInteractionListener, FragmentNews.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentQuestions.OnFragmentInteractionListener, FragmentPersonalArea.OnFragmentInteractionListener, FragmentNews.OnFragmentInteractionListener, FragmentCategories.OnFragmentInteractionListener {
+    private UserInfoStorage userInfoStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userInfoStorage = new UserInfoStorage(MainActivity.this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -50,6 +53,12 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        TextView usernameTextView = (TextView)findViewById(R.id.drawerUsernameTextView);
+        TextView namesTextView = (TextView)findViewById(R.id.drawerNameTextView);
+        usernameTextView.setText(userInfoStorage.getUsername());
+        namesTextView.setText(userInfoStorage.getNames());
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -108,6 +117,8 @@ public class MainActivity extends AppCompatActivity
             fragmentClass = FragmentPersonalArea.class;
         } else if (id == R.id.nav_questions) {
             fragmentClass = FragmentQuestions.class;
+        } else if (id == R.id.nav_categories) {
+            fragmentClass = FragmentCategories.class;
         }
         try {
             fragment = (Fragment) fragmentClass.newInstance();
